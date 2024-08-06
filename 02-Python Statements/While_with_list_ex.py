59534f8e8de0ad38d9b9343b49621b8a5b3524f2
Humaazmi <94289110+Humaazmi@users.noreply.github.com>
2024-08-06 18:22:31 +0300

    Create While_with_list_ex.py

---
+++ 02-Python Statements/While_with_list_ex.py
@@ -0,0 +1,9 @@
+l2=[18,19,20,21,22]
+index=0
+while index<len(l2):
+    num=l2[index]
+    if num%2==0:
+        print(f"The even number is {num}")
+    else:
+        print(f"The odd number is {num}")
+    index=index+1
